d94de34469042d2b34652eea99796c56ead3fbd2
Anastasiia Kostiv <akostiv11@gmail.com>
2024-11-13 16:00:45 +0200

    add missing dependency

--- dependencies.R
+++ dependencies.R
@@ -1,5 +1,6 @@
 # This file allows packrat (used by rsconnect during deployment) to pick up dependencies.
 library(bslib)
+library(OSPSuite-R)
 library(rhino)
 library(shiny.destroy)
 library(shiny.react)
