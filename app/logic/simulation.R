0f8a243acf4128d192f1e275057d80c9525c205f
Felix MIL <felix.mil@esqlabs.com>
2024-11-14 16:30:31 +0100

    Get all outputs

--- app/logic/simulation.R
+++ app/logic/simulation.R
@@ -8,5 +8,9 @@ load_simulation <- function(){
   message("Initializing Simulation")
   simulation <- ospsuite::loadSimulation("app/logic/coffee-sim.pkml")
   ospsuite::setOutputInterval(simulation, 0, 24*60-1, resolution = 1/2)
+  ospsuite::setOutputs(c("Organism|PeripheralVenousBlood|Caffeine|Plasma (Peripheral Venous Blood)",
+                         "Organism|Brain|Intracellular|Caffeine|Concentration",
+                         "Organism|Heart|Intracellular|Caffeine|Concentration"),
+                       simulation)
   return(simulation)
 }
